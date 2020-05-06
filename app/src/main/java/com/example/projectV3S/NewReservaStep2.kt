59e79e2a23b97c255db54cf02bfc2d00f2837417
julianzd13 <julian.zapatad@udea.edu.co>
2020-05-05 20:51:15 -0500

    V3 Step2

--- app/src/main/java/com/example/projectV3S/NewReservaStep2.kt
+++ app/src/main/java/com/example/projectV3S/NewReservaStep2.kt
@@ -3,10 +3,17 @@ package com.example.projectV3S
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import com.example.projectV3S.UTILS.Constantes
+import com.example.projectV3S.model.Usuario
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_new_reserva_step2.*
 
 
@@ -14,6 +21,7 @@ import kotlinx.android.synthetic.main.activity_new_reserva_step2.*
 class NewReservaStep2 : AppCompatActivity() {
 
     var num_particif1 : String = Constantes.EMPTY
+    var numpartici = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +35,11 @@ class NewReservaStep2 : AppCompatActivity() {
         SetearSpinnersyeditt()
 
         bt_conti_to_step3.setOnClickListener {
-            Comprobacionlayers()
+            numpartici = 0
+            var oknumpartici = Comprobacionlayers()
+            if (oknumpartici == num_particif1.toInt()){
+                Toast.makeText(this, "BIIIIENNN", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
@@ -82,10 +94,80 @@ class NewReservaStep2 : AppCompatActivity() {
         }
     }
 
-    private fun Comprobacionlayers() {
+    @SuppressLint("ResourceType")
+    private fun Comprobacionlayers(): Int {
+
+        et_user_num_cedu_step2_1.id = 21
+        et_user_num_cedu_step2_2.id = 22
+        et_user_num_cedu_step2_3.id = 23
+        et_user_num_cedu_step2_4.id = 24
+        et_user_num_cedu_step2_5.id = 25
+        et_user_num_cedu_step2_6.id = 26
+        et_user_num_cedu_step2_7.id = 27
+        et_user_num_cedu_step2_8.id = 28
+        et_user_num_cedu_step2_9.id = 29
+        et_user_num_cedu_step2_10.id = 30
+        et_user_num_cedu_step2_11.id = 31
+        et_user_num_cedu_step2_12.id = 32
+        et_user_num_cedu_step2_13.id = 33
+        et_user_num_cedu_step2_14.id = 34
+        et_user_num_cedu_step2_15.id = 35
+        et_user_num_cedu_step2_16.id = 36
 
 
+        for (j in 21..(20 + num_particif1.toInt())){
 
+            var vari1 : String= findViewById<EditText>(j).text.toString()
+            var vari2 : String
+            var flag = 0
+            if (vari1.isNotEmpty()){
+                for (k in (j + 1)..(20 + num_particif1.toInt())){
+                    vari2 = findViewById<EditText>(k).text.toString()
+                    if(vari2.isNotEmpty()){
+                        if (vari1 == vari2){
+                            Toast.makeText(this, getText(R.string.error_step2), Toast.LENGTH_SHORT).show()
+                            findViewById<EditText>(k).setText(Constantes.EMPTY)
+                            flag = 1
+                        }
+
+                    }
+                }
+                if(flag!= 1){
+                    var checkvar = verificacionfireb(vari1)
+                    if (checkvar == false) findViewById<EditText>(j).setText(Constantes.EMPTY)
+                    if (checkvar == true) numpartici +=1
+                }
+            }
+        }
+
+        return numpartici
+    }
+
+    private fun verificacionfireb(searchText: String?): Boolean {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("usuarios")
+        var existedocu = false
+
+        myRef.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (snapshot in dataSnapshot.children){
+                    val usertodocu = snapshot.getValue(Usuario::class.java)
+                    if (usertodocu!!.num_documento.equals(searchText)){
+                        existedocu = true
+
+                    }
+                }
+                if (!existedocu){
+                    Toast.makeText(this@NewReservaStep2, "NO existe Ususario", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("quepasomal", "Failed to read value", error.toException())
+            }
+        })
+        return existedocu
     }
 
 
